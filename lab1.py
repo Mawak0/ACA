849734c76bdea3596c97da609f8244240a4ef874
Mawak0 <a32221580@gmail.com>
2026-02-04 22:37:00 +0600

    upd

---
+++ lab1.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import time
+
+def iter_alg(n):
+    a, b = 0, 1
+    for i in range(2, n):
+        a, b = b, a+b
+    return b
+
+def recurs_alg(n, i=2, a=0, b=1):
+    if n == 1:
+        return 0
+    if i == n:
+        return b
+    return recurs_alg(n, i+1, b, a+b)
+
+def get_time_of_exec(func, arg):
+    start_time = time.perf_counter()
+    out = func(arg)
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    return out, elapsed_time
+
+calc_range = range(3, 40)
+recurs_times = [get_time_of_exec(recurs_alg, i)[1] for i in calc_range]
+iter_times = [get_time_of_exec(iter_alg, i)[1] for i in calc_range]
+
+
+plt.plot(recurs_times, label="Рекурсивный")
+plt.plot(iter_times, label="Итеративный")
+plt.legend(loc='upper left', fontsize='medium') # Добавление легенды
+plt.xlabel('Порядковый номер числа последовательности')
+plt. ylabel('Время в мс')
+plt.show()
