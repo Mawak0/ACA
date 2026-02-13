32b70fa71453d86591f91d22f4f7f6b0892a63f2
Mawak0 <a32221580@gmail.com>
2026-02-13 11:02:09 +0600

    upd

--- lab3.py
+++ lab3.py
@@ -2,11 +2,12 @@ import time
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+
 def narayana_permutations(arr):
     permutations = [arr[::]]
     arr = arr[::]
     while True:
-        max_i_arr = [i for i in range(0, len(arr)-1) if arr[i] < arr[i+1]]
+        max_i_arr = [i for i in range(0, len(arr) - 1) if arr[i] < arr[i + 1]]
         max_i_arr.append(-1)
         max_i = max(max_i_arr)
         if max_i == -1:
@@ -19,7 +20,7 @@ def narayana_permutations(arr):
                     find_min = arr[j]
                     find_id = j
         arr[find_id], arr[max_i] = arr[max_i], arr[find_id]
-        arr[max_i+1:] = arr[max_i+1:][::-1]
+        arr[max_i + 1:] = arr[max_i + 1:][::-1]
         permutations.append(arr[::])
 
 
@@ -27,12 +28,13 @@ def trotter_permutations(arr):
     arr = arr[::]
     permutations = [arr[::]]
     directions = [0 for i in range(0, len(arr))]
+
     def get_mobil_elems():
         mobil = []
         for r in range(0, len(arr)):
-            if directions[r] == 0 and r !=0 and arr[r-1] < arr[r]:
+            if directions[r] == 0 and r != 0 and arr[r - 1] < arr[r]:
                 mobil.append(r)
-            if directions[r] == 1 and r != len(arr)-1 and arr[r+1] < arr[r]:
+            if directions[r] == 1 and r != len(arr) - 1 and arr[r + 1] < arr[r]:
                 mobil.append(r)
         return mobil
 
@@ -43,13 +45,14 @@ def trotter_permutations(arr):
 
         def a(e):
             return arr[e]
+
         max_mobil = max(mobil_elems, key=a)
         if directions[max_mobil] == 0:
-            arr[max_mobil-1], arr[max_mobil] = arr[max_mobil], arr[max_mobil-1]
-            directions[max_mobil-1], directions[max_mobil] = directions[max_mobil], directions[max_mobil-1]
+            arr[max_mobil - 1], arr[max_mobil] = arr[max_mobil], arr[max_mobil - 1]
+            directions[max_mobil - 1], directions[max_mobil] = directions[max_mobil], directions[max_mobil - 1]
             max_mobil -= 1
         if directions[max_mobil] == 1:
-            arr[max_mobil+1], arr[max_mobil] = arr[max_mobil], arr[max_mobil+1]
+            arr[max_mobil + 1], arr[max_mobil] = arr[max_mobil], arr[max_mobil + 1]
             directions[max_mobil + 1], directions[max_mobil] = directions[max_mobil], directions[max_mobil + 1]
             max_mobil += 1
         permutations.append(arr[::])
@@ -59,11 +62,11 @@ def trotter_permutations(arr):
                     directions[i] = 1
                 elif directions[i] == 1:
                     directions[i] = 0
-        
+
 
 def inv_to_perm(inv):
     n = len(inv)
-    elems = list(range(1, n+1))
+    elems = list(range(1, n + 1))
     perm = []
     for i, v in enumerate(inv):
         perm.append(elems.pop(v))
@@ -71,18 +74,20 @@ def inv_to_perm(inv):
 
 
 def generate_permutations_by_inv(n):
-    inv = [0]*n
+    inv = [0] * n
+    permutations = []
     while True:
-        yield inv_to_perm(inv)
-        i = n-1
+        permutations.append(inv_to_perm(inv))
+        i = n - 1
         while i >= 0:
             inv[i] += 1
-            if inv[i] <= (n-1-i):
+            if inv[i] <= (n - 1 - i):
                 break
             inv[i] = 0
             i -= 1
         if i < 0:
             break
+    return permutations
 
 
 def get_time_of_exec(func, arg):
@@ -99,11 +104,20 @@ narayana_times = [get_time_of_exec(narayana_permutations, list(range(0, i)))[1] 
 trotter_times = [get_time_of_exec(trotter_permutations, list(range(0, i)))[1] for i in tqdm(calc_range)]
 inv_times = [get_time_of_exec(generate_permutations_by_inv, i)[1] for i in tqdm(calc_range)]
 
-
 plt.plot(narayana_times, label="Алгоритм Нарайана")
 plt.plot(trotter_times, label="Алгоритм Джонсона-Троттера")
 plt.plot(inv_times, label="Алгоритм вектора инверсий")
 plt.legend(loc='upper left', fontsize='medium')
 plt.xlabel('Размер входных данных')
-plt. ylabel('Время в сек.')
+plt.ylabel('Время в сек.')
 plt.show()
+
+# сгенерировать все возможные комбинации перестановок элементов
+print(narayana_permutations([1,2,3]))
+
+# задано n-элементов. Необходимо перебрать все возможные варианты выборки из этих элементов
+def samples(array, n):
+    per = trotter_permutations(array)
+    return set([p[0:n] for p in per])
+
+print(samples(["стол", "стул", "шкаф"], 2))
